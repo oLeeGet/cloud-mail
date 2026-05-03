f3d319573ad015b306f3e05f15dab2c145e1d827
oLeeGet <108821020+oLeeGet@users.noreply.github.com>
2026-05-03 19:24:44 +0800

    Set language from localStorage or default to empty

--- mail-vue/src/store/setting.js
+++ mail-vue/src/store/setting.js
@@ -7,7 +7,7 @@ export const useSettingStore = defineStore('setting', {
             r2Domain: '',
             loginOpacity: 1.00,
         },
-        lang: '',
+        lang: localStorage.getItem('language') || '',
     }),
     actions: {
 
